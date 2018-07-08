5069887577c2ea2831e95697f4488a4eda3873cd
Miguel Ibarra <miguel@digitec.ws>
2018-07-08 00:28:49 +0000

    use IAM role instead of ENV aws credentiasl, improve base domain handling in serverless file

--- modules/aws.js
+++ modules/aws.js
@@ -3,13 +3,6 @@
 const AWS = require('aws-sdk');
 
 const awsModule = (function (AWS) {
-    const awsConfig = {
-        "accessKeyId": process.env.SES_AWS_ACCESS_KEY_ID,
-        "secretAccessKey": process.env.SES_AWS_SECRET_ACCESS_KEY,
-        "region": process.env.SES_AWS_REGION
-    };
-
-    AWS.config.update(awsConfig);
 
     const getAws = () => AWS;
 
